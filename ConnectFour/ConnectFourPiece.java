22e8ddee369ad109a6a58d2e66d365835671a4b6
Zhen Li <devinzhenli@gmail.com>
2019-03-18 14:22:49 -0700

    got rid of deadcode

--- ConnectFour/ConnectFourPiece.java
+++ ConnectFour/ConnectFourPiece.java
@@ -7,10 +7,9 @@ public class ConnectFourPiece extends Piece {
 	public String name;
 	public ConnectFourPiece() {
 		super(0);
-		name = "jacky";
 	}
 	public ConnectFourPiece(int id) {
-		super(id);   // 1=white, 2=black
+		super(id);   // 1=yellow, 2=red
 	}
 
 	@Override
