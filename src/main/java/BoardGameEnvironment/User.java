298019699106c089a2f53b11bb471fa878f9be23
FelixFungus <felixfung97@gmail.com>
2019-03-18 16:02:46 -0700

    feat: add game win/lose statistics in User

--- src/main/java/BoardGameEnvironment/User.java
+++ src/main/java/BoardGameEnvironment/User.java
@@ -2,9 +2,32 @@ package BoardGameEnvironment;
 
 public class User {
   String name;
+  public int memoryWins;
+  public int memoryLoses;
+
+  public int connectfourWins;
+  public int connectfourLoses;
+
+  public int checkersWins;
+  public int checkersLoses;
+
+  public int battleshipWins;
+  public int battleshipLoses;
+
 
   public User(String name) {
     this.name = name;
+    this.memoryWins = 0;
+    this.memoryLoses = 0;
+
+    this.connectfourWins = 0;
+    this.connectfourLoses = 0;
+
+    this.checkersWins = 0;
+    this.checkersLoses = 0;
+
+    this.battleshipWins = 0;
+    this.battleshipLoses = 0;
   }
 
   public String getName() {
